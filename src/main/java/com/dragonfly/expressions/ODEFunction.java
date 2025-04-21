59304b59b824318f76c3aa2c0326279bf06b7202
Issven <rajveerjadhav08@gmail.com>
2025-04-21 23:46:57 +0200

    Created @SpaceObject Annotation.

--- src/main/java/com/dragonfly/expressions/ODEFunction.java
+++ src/main/java/com/dragonfly/expressions/ODEFunction.java
@@ -1,4 +1,8 @@
 package com.dragonfly.expressions;
 
 public interface ODEFunction {
+
+    double dishW0();
+
+    double dishODEValue();
 }
---
+++ src/main/java/com/dragonfly/main/SolarSystemSimulation.java
@@ -0,0 +1,18 @@
+package com.dragonfly.main;
+
+import com.dragonfly.celestialbodies.CelestialBody;
+
+import java.util.HashMap;
+
+public class SolarSystemSimulation {
+
+    public static void main(String[] args){
+
+        // Let's first set-up our Initial model of SolarSystem at time T0
+        // We will use Hash map to get the Planet and its CelestialBody class instance
+        HashMap<String, CelestialBody> SolarSystemMap = new HashMap<>();
+
+
+    }
+
+}
